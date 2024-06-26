728aab6a8e4793ef9da4224c2f67e117c0aabd6c
migougou <miguel.litche@espi.fr>
2024-06-26 22:02:54 +0200

    test de plusieurs components et ajout de flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,13 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
+    './node_modules/flowbite-react/lib/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 }
 
