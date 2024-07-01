7948bc8f06be48800eed5e025c9bf4d1186f36f2
migougou <miguel.litche@espi.fr>
2024-07-01 17:43:14 +0200

    Navigation navbar établie

---
+++ app/home/page.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Home(){
+  return (
+
+        <>
+        <h1>Bienvenue à tous</h1>
+        </>
+      )
+    }
--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,13 @@
 import './globals.css';
+import NavBar from './components/NavBar';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NavBar />
+        {children}
+        </body>
     </html>
   );
 }
